a969ae398e6bf1284ab96836608cb489fdc6f66d
Abid <ranaabid52001@outlook.com>
2024-01-31 23:16:27 +0500

    changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,110 @@
-<footer id="siteFooter" class="container-fluid site-footer">
+<style>
+.w-20 {
+    width: 20%;
+}
+
+.bg-footer {
+    background-color: var(--mainFooterColor);
+}
+
+.bg-footer .w-20 h5 {
+    color: #ffffff;
+    font-weight: 600;
+    line-height: 30px;
+}
+
+.bg-footer .w-20 li>a {
+    color: #ffffff;
+    opacity: 0.5;
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 34px;
+}
+
+.footer-border-top {
+    border-top: 2px solid #3e3b39;
+}
+
+.footer-border-top .footer-copy {
+    font-size: 13px;
+    font-weight: 400;
+    line-height: 34px;
+    color: #ffffff;
+    opacity: 0.5;
+}
+</style>
+<footer id="siteFooter" class="container-fluid site-footer bg-footer">
     <div class="container">
-        <div class="row">
+        <div class="d-flex">
+            <div class="w-20">
+                <h5>About Us</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Our Story</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Testimonials</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0"> Blog</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Sign In</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Live chat</a></li>
+                </ul>
+            </div>
+            <div class="w-20">
+                <h5>Categories</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Occasions</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Fruit</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Gourmet</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0"> Wine/Champagne</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Towers/Tins/Boxes</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Kosher</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Healthy</a></li>
+                </ul>
+            </div>
+            <div class="w-20">
+                <h5>Customer Service</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">FAQs</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Orders and Payment</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Shipping Information</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Return Policy</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Private Policy</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Accessibility</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="w-20">
+                <h5>Partner With Us</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Drop Ship</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Affiliates</a></li>
+                </ul>
+                <h5>Corporate Orders</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Volume Discounts</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Multi-Order Spreadsheet</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Corporate Gifts</a></li>
+                </ul>
+            </div>
+            <div class="w-20">
+                <h5>Sister Sites</h5>
+                <ul class="nav flex-column">
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">babybasket.com</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">winebasket.com</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Our New Jersey Gift Baskets Local
+                            Store</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">350 Allwood Road</a></li>
+                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Clifton, NJ 07012</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row footer-border-top ">
             <div class="col footer-col">
-                <p class="footer-copy">2024 PA Dutch Baskets. All rights reserved. Click here to view our refund & privacy policies. Click here to view our shipping policies.</p>
+                <p class="footer-copy">2024 PA Dutch Baskets. All rights reserved. Click here to view our refund &
+                    privacy policies. Click here to view our shipping policies.</p>
             </div>
         </div>
     </div>
 </footer>
-
-
-</div><!--/#content-->
+</div>
+<!--/#content-->
 
 <script src="<?= $app_path ?>assets/js/jquery-3.7.1.slim.js"></script>
 <script src="<?= $app_path ?>assets/js/bootstrap/bootstrap.bundle.js"></script>
