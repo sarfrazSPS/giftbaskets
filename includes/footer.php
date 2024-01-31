240bbdc9d8100c21ed37ed413cf31c0a78afc3dc
Ssattarcom <Sarfaraz.Sattar@spsnet.com>
2024-01-31 22:16:26 +0500

    latest

--- includes/footer.php
+++ includes/footer.php
@@ -2,22 +2,17 @@
     <div class="container">
         <div class="row">
             <div class="col footer-col">
-                <p class="footer-copy text-uppercase">BARNCO WOODWORKS LLC -- 301 S ROUTE 73 BERLIN NJ 08009------</p>
-                <p class="footer-copy">Barnco Swedesboro 2199 US-322, NJ 08085</p>
-                <p class="footer-copy">©, BarnCo Woodworks LLC. All Rights Reserved.</p>
+                <p class="footer-copy">2024 PA Dutch Baskets. All rights reserved. Click here to view our refund & privacy policies. Click here to view our shipping policies.</p>
             </div>
         </div>
     </div>
 </footer>
 
 
-</div>
+</div><!--/#content-->
 
 <script src="<?= $app_path ?>assets/js/jquery-3.7.1.slim.js"></script>
 <script src="<?= $app_path ?>assets/js/bootstrap/bootstrap.bundle.js"></script>
-<script type="text/javascript" src="<?= $app_path ?>assets/js/jquery.fancybox.min.js"></script>
-<link rel="stylesheet" href="<?= $app_path ?>assets/css/jquery.fancybox.min.css" type="text/css" media="screen" />
-
 </body>
 
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php
-$web_dir = "my_sites/barncoamishbuilders/";
+$web_dir = "my_sites/giftbaskets/";
 $app_path = "http://" . $_SERVER['HTTP_HOST'] . "/" . $web_dir;
 $root_path = $_SERVER['DOCUMENT_ROOT'] . "/" . $web_dir;
 
@@ -18,8 +18,21 @@ $current_year  = date("Y", strtotime($today));
     <link href="<?= $app_path ?>assets/css/style.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <div id="content" class="site-content">
+        
+        <section class="top-bar-section container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 top-bar-left">
+                        <a href="#">A Note About Food Safety and Shipping</a>
+                    </div>
+                    <div class="col-md-6 top-bar-right">
+                        <a href="#">View Our 2023-24 Holiday Catalogue</a>
+                    </div>
+                </div>
+            </div>
+        </section>
