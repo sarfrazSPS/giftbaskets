9b399b2034e3845f86844ab5d2c238196399f1ac
Ssattarcom <Sarfaraz.Sattar@spsnet.com>
2024-01-31 23:17:38 +0500

    working on header

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,5 @@
 <?php
-$web_dir = "my_sites/giftbaskets/";
+$web_dir = "my_sites/padutchbaskets/";
 $app_path = "http://" . $_SERVER['HTTP_HOST'] . "/" . $web_dir;
 $root_path = $_SERVER['DOCUMENT_ROOT'] . "/" . $web_dir;
 
@@ -40,14 +40,40 @@ $current_year  = date("Y", strtotime($today));
         <section class="branding-section container-fluid">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-4 branding-left">
-                        <a href="#">A Note About Food Safety and Shipping</a>
+
+                    <div class="col-md-4 d-flex align-items-center branding-left">
+                        <div class="branding-left-content">
+                            <div class="blc-social">
+                                <a href="#" class="d-inline-block">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
+                                </a>
+                                <a href="#" class="d-inline-block">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/></svg>
+                                </a>
+                                <a href="#" class="d-inline-block">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M204 6.5C101.4 6.5 0 74.9 0 185.6 0 256 39.6 296 63.6 296c9.9 0 15.6-27.6 15.6-35.4 0-9.3-23.7-29.1-23.7-67.8 0-80.4 61.2-137.4 140.4-137.4 68.1 0 118.5 38.7 118.5 109.8 0 53.1-21.3 152.7-90.3 152.7-24.9 0-46.2-18-46.2-43.8 0-37.8 26.4-74.4 26.4-113.4 0-66.2-93.9-54.2-93.9 25.8 0 16.8 2.1 35.4 9.6 50.7-13.8 59.4-42 147.9-42 209.1 0 18.9 2.7 37.5 4.5 56.4 3.4 3.8 1.7 3.4 6.9 1.5 50.4-69 48.6-82.5 71.4-172.8 12.3 23.4 44.1 36 69.3 36 106.2 0 153.9-103.5 153.9-196.8C384 71.3 298.2 6.5 204 6.5z"/></svg>
+                                </a>
+                            </div>
+                            <div class="blc-contact clearfix">
+                                <div class="blcc-left">
+                                    <img src="<?=$app_path;?>assets/images/mobile.png" />
+                                </div>
+                                <div class="blcc-rigt">
+                                    <p class="blccr-1">717-368-3973</p>
+                                    <p class="blccr-2"><a href="mailto:info@padutchbaskets.com">Email - info@padutchbaskets.com</a></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="col-md-4 branding-center">
-                        <a href="#">View Our 2023-24 Holiday Catalogue</a>
+
+                    <div class="col-md-4 d-flex align-items-center branding-center">
+                        <a href="#" class="d-inline-block">
+                            <img src="<?=$app_path;?>assets/images/logo.png" class="image-fluid" />
+                        </a>
                     </div>
-                    <div class="col-md-4 branding-right">
-                        <a href="#">View Our 2023-24 Holiday Catalogue</a>
+
+                    <div class="col-md-4 d-flex align-items-center branding-left">
+                        <div class="branding-right-content"></div>
                     </div>
                 </div>
             </div>
