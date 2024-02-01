8c3fb1a4dee542a88d78027baca2663fa62d4e78
Ssattarcom <Sarfaraz.Sattar@spsnet.com>
2024-02-01 21:57:20 +0500

    hero section

--- index.php
+++ index.php
@@ -1,6 +1,67 @@
 <?php include("includes/header.php"); ?>
 
-<section class="pt-5 pb-5">
+<section id="productCats" class="product-cats">
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 proc-left">
+                    <h3>Product Categories</h3>                   
+                    <div class="ui-accordion">
+                        <div class="ui-accordion-item">
+                            <a href="javascript:void(0);" class="ui-accordion-title">
+                                <span>Occasion</span>
+                            </a>
+                            <div class="ui-accordion-content">
+                                <ul class="category-selection">
+                                    <li>
+                                        <a href="#" class="acc-cs-link">Anniversary <span>(5)</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="acc-cs-link">Birthday <span>(9)</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="acc-cs-link">Congratulations <span>(16)</span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="acc-cs-link">Corporate <span>(28)</span></a>
+                                    </li>
+                                    <li>
+                                    <a href="#" class="acc-cs-link">Get Well <span>(9)</span></a>
+                                    </li>
+                                    <li>
+                                    <a href="#" class="acc-cs-link">Housewarming <span>(26)</span></a>
+                                    </li>
+                                    <li>
+                                    <a href="#" class="acc-cs-link">New Baby <span>(1)</span></a>
+                                    </li>
+                                    <li>
+                                    <a href="#" class="acc-cs-link">Retirement <span>(13)</span></a>
+                                    </li>
+                                    <li>
+                                    <a href="#" class="acc-cs-link">Sympathy <span>(8)</span></a>
+                                    </li>
+                                    <li>
+                                    <a href="#" class="acc-cs-link">Thank You Gifts <span>(15)</span></a>
+                                    </li>
+                                    <li>
+                                    <a href="#" class="acc-cs-link">Valentine's Day <span>(3)</span></a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-9 proc-right">
+                    <img src="<?=$app_path;?>assets/images/hero.png" class="img-fluid" >
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="testimonials" class="pt-5 pb-5">
     <div class="container-fluid testimonial-head">
         <div class="container">
             <div class="row text-center mb-5">
