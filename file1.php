48020b15f0a1e69b35d4fc0b116fd85d476248b8
Ssattarcom <Sarfaraz.Sattar@spsnet.com>
2024-02-01 23:56:01 +0500

    occasion section

---
+++ file1.php
@@ -0,0 +1,44 @@
+<?php
+$app_path="http://localhost//my_sites/padutchbaskets/";
+?>
+<section id="OccasionSeciton" class="occs-section">
+    <div class="container-fluid occss-bg">
+        <div class="occssbg-overlay"></div>
+        <div class="container occss-container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Shop for any Occassion</h2>
+                    <div class="occss-content">
+                        <div class="row">
+                            <div class="col-md-2 occss-item">
+                                <img src="<?=$app_path;?>assets/images/graphics/thankyou-full.png" />
+                                <p>Thank you</p>
+                            </div>
+                            <div class="col-md-2 occss-item">
+                                <img src="<?=$app_path;?>assets/images/graphics/birthday-full.png" />
+                                <p>Birthday</p>
+                            </div>
+                            <div class="col-md-2 occss-item">
+                                <img src="<?=$app_path;?>assets/images/graphics/sympathy-full.png" />
+                                <p>Sympathy</p>
+                            </div>
+                            <div class="col-md-2 occss-item">
+                                <img src="<?=$app_path;?>assets/images/graphics/thankyou-full.png" />
+                                <p>Get Well</p>
+                            </div>
+                            <div class="col-md-2 occss-item">
+                                <img src="<?=$app_path;?>assets/images/graphics/birthday-full.png" />
+                                <p>Business</p>
+                            </div>
+                            <div class="col-md-2 occss-item">
+                                <img src="<?=$app_path;?>assets/images/graphics/sympathy-full.png" />
+                                <p>Congratulations</p>
+                            </div>
+                        </div>                        
+                    </div>
+                    <h4>Don’t see your occasion or event? Call us direct</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
