90f0bdeefc60abab5ef4e01229f30c09e8f6a24f
Ssattarcom <Sarfaraz.Sattar@spsnet.com>
2024-02-03 23:30:48 +0500

    latest

--- includes/header.php
+++ includes/header.php
@@ -34,10 +34,10 @@ $current_year  = date("Y", strtotime($today));
         <section class="top-bar-section container-fluid">
             <div class="container">
                 <div class="row">
-                    <div class="col-sm-6 col-md-6 top-bar-left">
+                    <div class="col-sm-6 col-md-6 col-6 top-bar-left">
                         <a href="#">A Note About Food Safety and Shipping</a>
                     </div>
-                    <div class="col-sm-6 col-md-6 top-bar-right">
+                    <div class="col-sm-6 col-md-6 col-6 top-bar-right">
                         <a href="#">View Our 2023-24 Holiday Catalogue</a>
                     </div>
                 </div>
