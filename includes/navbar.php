ac483310b92ad23a8a7913aeca43f082af0eeb68
Ssattarcom <Sarfaraz.Sattar@spsnet.com>
2024-02-27 19:53:59 +0500

    cart icon added and made responsive

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,10 @@
 <section class="site-navigation container">
+    <div class="cart-icon-header cart-for-sm">
+        <a href="<?=$app_path;?>cart.php">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+            <span class="cart-item-header">12</span>
+        </a>
+    </div>
     <nav class="navbar navbar-expand-md navbar-light bg-light">
         <a class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon" id="navbarIcon"></span>
@@ -29,6 +35,12 @@
                     <a class="nav-link" href="<?=$app_path;?>about.php">About</a>
                 </li>
             </ul>
+            <div class="cart-icon-header">
+                <a href="<?=$app_path;?>cart.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/></svg>
+                    <span class="cart-item-header">12</span>
+                </a>
+            </div>
         </div>
     </nav>
 </section>
